00386af6060a4167c2c7aaec8f2d6b3ef030d6e9
harbby2024 <harbby.a.t@gmail.com>
2025-04-07 23:53:17 -0500

    cambios 2

--- pages/1_📌_M2 Actvidad 1.py
+++ pages/1_📌_M2 Actvidad 1.py
@@ -22,7 +22,10 @@ st.markdown("Familiarizarse con la creación de DataFrames en Pandas y mostrarlo
 
 st.header("DataFrames desde diferentes fuentes:")
 st.subheader("DataFrames desde Diccionario")
-info_libros ={"Titulo": ["Harry Potter y la piedra filosofa", "Lucky boy", "Atravez de ti","La Riqueza de las naciones"]}, { "Autor": ["J. K. Rowling", "Shanthi Sekeran", "Ariana Godoy", "Adam Smith "] }, { "Año de publicación":[2000, 2017, 2021, 1992] }, {"Genero":["Ficción", "Drama" , "Romantica" , "Política" ]},
+info_libros ={"Titulo": ["Harry Potter y la piedra filosofa", "Lucky boy", "Atravez de ti","La Riqueza de las naciones"], 
+            "Autor":["J. K. Rowling", "Shanthi Sekeran", "Ariana Godoy", "Adam Smith "], 
+            "Año de publicación":[2000, 2017, 2021, 1992], 
+            "Genero":["Ficción", "Drama" , "Romantica" , "Política" ]}
 
 df_libros = pd.DataFrame(info_libros)
 st.write(df_libros)

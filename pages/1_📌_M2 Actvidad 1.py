7f36f0bad41c7712fcddf159dbfb978dcbf9ea2f
harbby2024 <harbby.a.t@gmail.com>
2025-04-07 23:45:19 -0500

    cambio

--- pages/1_📌_M2 Actvidad 1.py
+++ pages/1_📌_M2 Actvidad 1.py
@@ -22,16 +22,23 @@ st.markdown("Familiarizarse con la creación de DataFrames en Pandas y mostrarlo
 
 st.header("DataFrames desde diferentes fuentes:")
 st.subheader("DataFrames desde Diccionario")
-info_libros ={"Titulo": ["Harry Potter y la piedra filosofa", "Lucky boy", "Atravez de ti","La Riqueza de las naciones"]},
-            { "Autor": ["J. K. Rowling", "Shanthi Sekeran", "Ariana Godoy", "Adam Smith "] },
-            { "Año de publicación":[2000, 2017, 2021, 1992] },
-            {"Genero":["Ficción", "Drama" , "Romantica" , "Política" ]},
+info_libros ={"Titulo": ["Harry Potter y la piedra filosofa", "Lucky boy", "Atravez de ti","La Riqueza de las naciones"]}, { "Autor": ["J. K. Rowling", "Shanthi Sekeran", "Ariana Godoy", "Adam Smith "] }, { "Año de publicación":[2000, 2017, 2021, 1992] }, {"Genero":["Ficción", "Drama" , "Romantica" , "Política" ]},
 
 df_libros = pd.DataFrame(info_libros)
 st.write(df_libros)
 st.write(df_libros.describe())
 
+st.subheader("DataFrames desde Lista de Diccionario")
 
+poblacion_ciudades =[{"Nombre":"Tokio", "Población": "37,3 millones de habitantes.", "Pais":"Japón"},
+            {"Nombre":"Delhi", "Población": "29,3 millones de habitantes. ​", "Pais":"India" },
+            {"Nombre":"Shanghái", "Población": "26,3 millones de habitantes.", "Pais":"China" },
+            {"Nombre":"São Paulo", "Población": "22 millones de habitantes.", "Pais":"Brasil"},]
+
+df_poblacion_ciudades = pd.DataFrame(poblacion_ciudades)
+
+st.title("Población de las ciudades más grandes del mundo")
+st.dataframe(df_poblacion_ciudades.info)
 
 
 

deb4fe165635e5314e888c52cb07e7bd0f5452a1
harbby2024 <harbby.a.t@gmail.com>
2025-04-08 00:08:45 -0500

    camb

--- pages/1_📌_M2 Actvidad 1.py
+++ pages/1_📌_M2 Actvidad 1.py
@@ -17,6 +17,8 @@ tipos de datos, operadores, y las estructuras de datos más utilizadas como list
 diccionarios y conjuntos.
 """)
 
+#Actividad 1 punto 01 Dataframes Diccionario
+
 st.header("Objetivo de la actividad")
 st.markdown("Familiarizarse con la creación de DataFrames en Pandas y mostrarlos usando Streamlit.")
 
@@ -24,12 +26,14 @@ st.title("DataFrames desde diferentes fuentes:")
 st.subheader("DataFrames desde Diccionario")
 info_libros ={"Titulo": ["Harry Potter y la piedra filosofa", "Lucky boy", "Atravez de ti","La Riqueza de las naciones"], 
             "Autor":["J. K. Rowling", "Shanthi Sekeran", "Ariana Godoy", "Adam Smith "], 
-            "Año de publicación":["2000", "2017", "2021", "1992"], 
+            "Año de publicación":[2000, 2017, 2021, 1992], 
             "Genero":["Ficción", "Drama" , "Romantica" , "Política" ]}
 
 df_libros = pd.DataFrame(info_libros)
 st.write(df_libros)
 st.write(df_libros.describe())
+
+#Actividad 1 punto 02 Dataframes lista de Diccionario
 
 st.subheader("DataFrames desde Lista de Diccionario")
 
@@ -43,5 +47,19 @@ df_poblacion_ciudades = pd.DataFrame(poblacion_ciudades)
 st.subheader("Población de las ciudades más grandes del mundo")
 st.dataframe(df_poblacion_ciudades)
 
+#Actividad 1 punto 03 Lista de Lista
 
+st.subheader("DataFrames desde lista de lista")
+
+inventario = [["sudadera manga larga", 20.000, 60],
+            ["Jogger dama", 150.000, 120]
+            ["Camiseta tipo polo", 90.000, 73]
+            ]
+
+df_inventario = pd.DataFrame(inventario, columns=["Nombre de producto", "Precio", "Stock"])
+st.write(df_inventario)
+
+#nombre del producto, precio y cantidad en stock
+#Convierte esta lista en un DataFrame con Pandas, especificando los nombres de las columnas (pista: usa el parámetro columns en la función de creación).
+#En Streamlit, añade un texto como "Productos en Inventario" y muestra el DataFrame con st.dataframe().
 

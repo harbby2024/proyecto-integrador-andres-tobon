4e5ef75a1febd5663112318084f6a071198a79f9
harbby2024 <harbby.a.t@gmail.com>
2025-04-25 01:48:58 -0500

    jjjj

--- pages/1_📌_M2 Actvidad 1.py
+++ pages/1_📌_M2 Actvidad 1.py
@@ -335,7 +335,7 @@ st.subheader("DataFrames desde Base de datos SQLite")
 st.subheader("DataFrames desde MongoDB")
 db_password = os.getenv("MONGO_PASSWORD")  # Ensure the password is set in your environment variables
 connection_string = f"mongodb+srv://harbby:{db_password}@cluster0.f5ft4fz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-clientes = pymongo.MongoClient("mongodb+srv://usuario:contraseña@cluster0.mongodb.net/?retryWrites=true&w=majority")
+clientes = MongoClient("mongodb+srv://usuario:contraseña@cluster0.mongodb.net/?retryWrites=true&w=majority")
 
 # Seleccionar la base de datos y la colección
 db = clientes["clientes_data"]  # Nombre de la base de datos

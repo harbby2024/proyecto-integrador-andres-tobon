208553f134d42114d05f837fcdfa7dcdf3f359de
harbby2024 <harbby.a.t@gmail.com>
2025-05-12 01:13:07 -0500

    dfdf

--- pages/6_📋_M2 Evaluación.py
+++ pages/6_📋_M2 Evaluación.py
@@ -6,7 +6,7 @@ st.markdown(
     """
     <a href="https://datostecnologiaexamgit-m7rqr8udpqqmqdmlfampyf.streamlit.app/" target="_blank">
         <button style='font-size:16px;padding:10px 20px;border-radius:8px;background-color:#FF4B4B;color:white;border:none;'>
-            Ir a revisar Examen 2 momento 
+            Ir a revisar Evaluación 2 momento 
         </button>
     </a>
     """,

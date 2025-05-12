e6a106dbe99660491f583416c1363a7c8faa0b93
harbby2024 <harbby.a.t@gmail.com>
2025-05-12 01:01:56 -0500

    adad

--- pages/6_📋_M2 Evaluación.py
+++ pages/6_📋_M2 Evaluación.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title("Momento 2 - Evaluación")
 
-if st.button("Abrir Evaluación 2 momento"):
-    st.markdown("[Redirigiendo...](https://datostecnologiaexamgit-m7rqr8udpqqmqdmlfampyf.streamlit.app/)", unsafe_allow_html=True)
+if st.button("Abrir Evaluación 2 momento(https://datostecnologiaexamgit-m7rqr8udpqqmqdmlfampyf.streamlit.app/)"):
+    st.markdown("[Redirigiendo...]", unsafe_allow_html=True)

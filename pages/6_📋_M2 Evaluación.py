9704828d5d8f745f6fa88b1cc14b1d7efc9fce3a
harbby2024 <harbby.a.t@gmail.com>
2025-05-12 00:56:08 -0500

    subir

--- pages/6_📋_M2 Evaluación.py
+++ pages/6_📋_M2 Evaluación.py
@@ -1,3 +1,6 @@
 import streamlit as st
 
 st.title("Momento 2 - Evaluación")
+
+st.header("Click en el enlace para ver Evaluacion")
+st.markdown("[Evaluación Momento 2](https://datostecnologiaexamgit-m7rqr8udpqqmqdmlfampyf.streamlit.app/)")
